16cb8c420c9da749d3d952d355f4f9087a2ec651
victorambrozi <victorambrozi.dev@gmail.com>
2022-01-28 13:46:29 -0300

    criaçao de bando de dados e captura e inserçao de dados

--- pages/chat.js
+++ pages/chat.js
@@ -1,21 +1,57 @@
-import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import { Box, TextField } from "@skynexui/components";
 import React, { useState } from "react";
 
+// local components
 import Header from "../components/Header/Header";
 import MessageList from "../components/MessageList/MessageList";
+
+// style
 import { styleChat } from "../styles/Chat";
+
+import { createClient } from "@supabase/supabase-js";
+
+// BACKEND
+const SUPABASE_ANON_PUBLIC =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM3MTA0MywiZXhwIjoxOTU4OTQ3MDQzfQ.pi1JnrtQmtfHmQpIP6Ph5Oqf8FORiNGQQqR7TbiWrrI";
+const SUPABASE_URL = "https://qlrjlrgsrexldsqfdpwm.supabase.co";
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_PUBLIC);
 
 export default function ChatPage() {
   const [mensagem, setMensagem] = useState("");
   const [messageList, setMessageList] = React.useState([]);
 
-  const handleNewMessage = (newMessage) => {
+  const getDataBase = () => {
+    supabaseClient
+      .from("mensagens")
+      .select("*")
+      .order("id", { ascending: false }) // inverte a ordem das mensagens
+      .then(({ data }) => {
+        setMessageList(data);
+      });
+  };
+
+  const insertDataBase = (messageObject) => {
+    supabaseClient
+      .from("mensagens")
+      .insert([messageObject]) // inserir um novo objeto com os dados da mensagem no banco de dados
+      .then(({ data }) => {
+        setMessageList([data[0], ...messageList]);
+      });
+  };
+
+  React.useEffect(() => {
+    // Evita de fazer a requisição, toda vez que uma tecla é acionada no input
+    getDataBase();
+  }, []); // a renderização é feita somente quando a pagina carrega (1 vez só)
+
+  const NewMessage = (newMessage) => {
     const message = {
-      id: messageList.length + 1,
       from: "victorambrozi",
       text: newMessage,
     };
-    setMessageList([message, ...messageList]);
+    // inserir no bando e dados
+    insertDataBase(message);
+
     setMensagem(""); // limpa input
   };
 
@@ -28,9 +64,10 @@ export default function ChatPage() {
 
     if (key === "Enter") {
       event.preventDefault();
-      handleNewMessage(mensagem);
+      NewMessage(mensagem);
     }
   };
+
   return (
     <Box styleSheet={styleChat.bgChat}>
       <Box styleSheet={styleChat.chatContainer}>
@@ -52,7 +89,6 @@ export default function ChatPage() {
     </Box>
   );
 }
-
 
 // Usuário
 // - usuário digita no textarea
